49e27aaf6f8c78a644dd700b1558f26e6845b81d
Jihad <kaiumjihad5@gmail.com>
2026-05-11 23:23:46 +0600

    Done 12 videos

---
+++ pg14.py
@@ -0,0 +1,16 @@
+#video 21
+
+'''
+i =1 #initialization
+while i <= 10: #condition
+print(i)
+i=i+1 #increment
+print("End")
+'''
+
+i=1
+var = input("Enter your name: ")
+
+while i <= 100:
+    print(var + " is :" , i)
+    i = i+3
