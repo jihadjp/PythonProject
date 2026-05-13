de4f9c8320994dac774f98b97a3d2f0410052f97
Jihad <kaiumjihad5@gmail.com>
2026-05-13 23:52:52 +0600

    ok

---
+++ pg15.py
@@ -0,0 +1,10 @@
+
+i=1
+while i<=100:
+    print(i)
+    i=i+1+i 
+    if i==20:
+        break
+    print("Here 20")
+
+print("Hello JP")
---
+++ pg23.py
@@ -0,0 +1,13 @@
+friends = ["Jihad", "Sakib", "Somrat", "Sabbir"]
+uni = ["DIU", "JU", "DIU", "AMC"]
+year = [2028, 2027, 2026, 2027]
+
+# i=0
+# while i<4:
+#     print(friends[i], "Will be graduated from", uni[i], "in", year[i],".")
+#     i=i+1
+
+
+print("Jihad" not in friends)
+print("2028" in uni)
+print(2027 in year)
